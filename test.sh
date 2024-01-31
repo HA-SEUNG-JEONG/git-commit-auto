fd07c14085568e462f52d6e6a386117af2219b72
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-01-31 22:30:49 +0900

    수정

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+# 사용자로부터 커밋 메시지를 입력 받음
+read -p "Enter commit message: " commit_message
 
-echo "hello world"
+# 모든 변경 사항을 스테이징
+git add .
+
+# 입력 받은 커밋 메시지로 커밋
+git commit -m "$commit_message"
+
+# 현재 브랜치 이름을 가져옴
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# 변경 사항을 원격 저장소로 푸시
+git push origin $current_branch
