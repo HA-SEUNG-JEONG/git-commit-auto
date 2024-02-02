bd871198e6daac1f2eefaf11ae8a9dc185b2baf6
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-02-02 23:21:48 +0900

    Test2

--- test.sh
+++ test.sh
@@ -6,6 +6,10 @@ read -p "Enter commit message: " commit_message
 # 모든 변경 사항을 스테이징
 git add .
 
+# 특정 파일만 스테이징
+read -p "Enter file to stage: " file_path
+git add "$file_path"
+
 # 입력 받은 커밋 메시지로 커밋
 git commit -m "$commit_message"
 
